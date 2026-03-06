723afe409ac36896e031e221aadc88e8078f84ad
CodeByD3v <ngrok6@gmail.com>
2026-03-06 23:36:40 +0530

    initialized FastAPI app with auth router

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.api import auth_routes
+
+app = FastAPI(title="Secure File System", description="Secure File System API", version="1.0.0")
+
+app.include_router(auth_routes.router, prefix="/auth")
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Secure File System API"}
+
+@app.get("/message")
+def get_message():
+    return {"message": "Welcome to the Secure File System API"}
+
